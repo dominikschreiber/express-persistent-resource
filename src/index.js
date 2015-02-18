afc2276da3445e1202608f5e769aaa8436c7369a
Dominik Schreiber <domischreib@gmail.com>
2015-02-18 11:34:45 +0100

    added CRUD router at GET /, POST /, GET /:id, PUT /:id

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 'use strict';
 
-module.exports = function(db, options) {
+module.exports = function(name, db, options) {
     var _ = require('lodash')
-      , configuration = _.extend({}, options)
-      , service = require('./service')(db)
+      , configuration = _.extend({view: name}, options)
+      , service = require('./service')(db, configuration)
       , router = require('./router')(service)
       , middleware = require('express').Router();
+    
+    middleware.get('/', router.list);
+    middleware.post('/', router.create);
+    middleware.get('/:id', router.read);
+    middleware.put('/:id', router.update);
     
     return middleware;
 };
