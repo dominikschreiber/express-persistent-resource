de2bf1ede1c3798aa69bb3297c9b33ad6535d04a
Dominik Schreiber <domischreib@gmail.com>
2015-02-18 15:52:15 +0100

    added tests for error/list/create/read/update

--- test/router.spec.js
+++ test/router.spec.js
@@ -1,7 +1,89 @@
 'use strict';
 
+var assert = require('assert')
+  , mock = {
+        findAll: function(next) {
+            next(null, [1,2,3]);
+        },
+        findById: function(id, next) {
+            next(null, {id: id, content: 'foo'});
+        },
+        save: function(data, next) {
+            next(null, data.id || 7);
+        }
+    }
+  , router = require('../src/router')(mock);
+
 describe('router', function() {
     describe('#error(res,err,next)', function() {
-        
+        it('should respond with a "500 internal server error" on errors', function(done) {
+            var err = 'this is an error' // creating an error here would make mocha fail
+              , res = {
+                    status: function(code) {
+                        assert.equal(500, code);
+                        return res;
+                    },
+                    json: function(e) {
+                        assert.equal(err, e);
+                        return res;
+                    }
+                };
+            router.error(res, err, function() {
+                // mocha would fail when done being called with error
+                // => make sure it is called without parameters
+                done();
+            });
+        });
+    });
+    
+    describe('#list(req,res,next)', function() {
+        it('should respond with a list of urls relative to /', function(done) {
+            router.list({
+                baseUrl: '/api/v1/test'
+            }, {
+                json: function(e) {
+                    assert.deepEqual(['/api/v1/test/1','/api/v1/test/2','/api/v1/test/3'], e);
+                }
+            }, done);
+        });
+    });
+    
+    describe('#create(req,res,next)', function() {
+        it('should respond with the url of the newly created entry', function(done) {
+            router.create({
+                body: {foo: 'bar'},
+                baseUrl: '/api/v1/test'
+            }, {
+                json: function(e) {
+                    assert.equal('/api/v1/test/7', e);
+                }
+            }, done);
+        });
+    });
+    
+    describe('#read(req,res,next)', function() {
+        it('should respond with the entry specified by id', function(done) {
+            router.read({
+                params: {id: 5}
+            }, {
+                json: function(e) {
+                    assert.deepEqual({id: 5, content: 'foo'}, e);
+                }
+            }, done);
+        });
+    });
+    
+    describe('#update(req,res,next)', function() {
+        it('should respond with the url of the updated entry', function(done) {
+            router.update({
+                body: {goo: 'gle'},
+                params: {id: 5},
+                baseUrl: '/api/v1/test'
+            }, {
+                json: function(e) {
+                    assert.equal('/api/v1/test/5', e);
+                }
+            }, done);
+        });
     });
 });
