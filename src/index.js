179f206069a80b3e0ecf5e55f655d7a0dc66f212
Dominik Schreiber <domischreib@gmail.com>
2015-02-19 09:47:58 +0100

    added forwarding of options to service/router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@
 module.exports = function(name, db, options) {
     var _ = require('lodash')
       , configuration = _.extend({view: name}, options)
-      , service = require('./service')(db, configuration)
-      , router = require('./router')(service)
+      , service = require('./service')(db, {
+            view: configuration.view
+            fields: configuration.fields,
+            id: configuration.id
+        })
+      , router = require('./router')(service, {
+            fields: configuration.fields
+        })
       , middleware = require('express').Router();
-    
+
     middleware.get('/', router.list);
     middleware.post('/', router.create);
     middleware.get('/:id', router.read);
     middleware.put('/:id', router.update);
-    
+
     return middleware;
 };
