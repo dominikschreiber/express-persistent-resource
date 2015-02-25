a4a6d2e21360449a2fa458e38d6b204f13d7fbc0
Dominik Schreiber <domischreib@gmail.com>
2015-02-25 10:42:10 +0100

    renamed field race to type

--- test/examples/cats.js
+++ test/examples/cats.js
@@ -3,8 +3,9 @@
 var app = require('express')()
   , resource = require('../../src/index');
 
+app.use(require('body-parser').json());
 app.use('/api/v1/cat', resource('cat', require('nano')('http://localhost:5984/animals'), {
-    fields: 'name,race,fur,food',
+    fields: 'name,type,fur,food',
     id: function(doc) {
         return doc.name.toLowerCase().replace(/[\w_]/g, '-');
     }
