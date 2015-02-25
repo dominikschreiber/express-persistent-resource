0811a449027a993b17b5c683aa910173df67132d
Dominik Schreiber <domischreib@gmail.com>
2015-02-25 12:20:26 +0100

    replaced \w in regex with \W *walks away whistling*

--- test/examples/cats.js
+++ test/examples/cats.js
@@ -7,7 +7,7 @@ app.use(require('body-parser').json());
 app.use('/api/v1/cat', resource('cat', require('nano')('http://localhost:5984/animals'), {
     fields: 'name,type,fur,food',
     id: function(doc) {
-        return doc.name.toLowerCase().replace(/[\w_]/g, '-');
+        return doc.name.toLowerCase().replace(/[\W_]/g, '-');
     }
 }));
 
